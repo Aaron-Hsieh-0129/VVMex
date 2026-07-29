49bb54cc7b525a3f34192ab0f0723cbd5b2a7f1a
Aaron@blaze <zz85721@gmail.com>
2026-07-29 17:43:52 +0800

    feat(AreaMeanNudge,LaterBoundaryNudging): ERA5 nudging for areameanwind and lateral boundary layer.

--- src/dynamics/forcings/AreaMeanNudging.hpp
+++ src/dynamics/forcings/AreaMeanNudging.hpp
@@ -4,6 +4,7 @@
 #include "core/Grid.hpp"
 #include "core/Parameters.hpp"
 #include "utils/ConfigurationManager.hpp"
+#include <Kokkos_Core.hpp>
 #include <string>
 
 #if defined(ENABLE_NCCL)
@@ -23,6 +24,7 @@ public:
     ~AreaMeanNudging() = default;
 
     void initialize(Core::State& state);
+    void update_forcing_target(Core::State& state, VVM::Real current_time);
 
     void apply_vorticity(Core::State& state, VVM::Real dt);
     void apply_uvtopmn(Core::State& state, VVM::Real dt);
@@ -37,6 +39,28 @@ private:
     VVM::Real nudgelim_;
     
     VVM::Real inv_total_xy_pts_;
+
+    bool use_netcdf_target_ = false;
+    bool constant_upper_wind_ = false;
+    std::string forcing_directory_;
+    std::string forcing_file_prefix_;
+    VVM::Real forcing_interval_s_ = 3600.0;
+    VVM::Real constant_upper_wind_threshold_Pa_ = 3000.0;
+    VVM::Real time_T1_ = 0.0;
+    VVM::Real time_T2_ = 3600.0;
+
+    Kokkos::View<VVM::Real*> u_T1_;
+    Kokkos::View<VVM::Real*> u_T2_;
+    Kokkos::View<VVM::Real*> v_T1_;
+    Kokkos::View<VVM::Real*> v_T2_;
+
+    std::string forcing_filename(VVM::Real time) const;
+    void load_wind_profiles(
+        const std::string& filename,
+        VVM::Real expected_time,
+        Kokkos::View<VVM::Real*>& u_target,
+        Kokkos::View<VVM::Real*>& v_target) const;
+    void check_nc_error(int status, const std::string& message) const;
 
 #if defined(ENABLE_NCCL)
     ncclComm_t nccl_comm_; 
