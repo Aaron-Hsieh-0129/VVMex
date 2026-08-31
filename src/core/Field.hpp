f32ea9a5a6fb397491051133102d9da98a49f2a6
Aaron@blaze <b08209006@ntu.edu.tw>
2026-08-31 23:54:43 +0800

    feat: add generalized coordinate abstraction classes

--- src/core/Field.hpp
+++ src/core/Field.hpp
@@ -7,27 +7,12 @@
 
 #include <Kokkos_Core.hpp>
 #include <type_traits>
-#include "Grid.hpp"
+
+#include "core/Grid.hpp"
+#include "core/GridStaggering.hpp"
 
 namespace VVM {
 namespace Core {
-
-enum class GridStaggering {
-    Unspecified, NotApplicable,
-
-    Centered,       // (i,     j,     k)
-
-    StaggeredX,     // (i+1/2, j,     k)     : u
-    StaggeredY,     // (i,     j+1/2, k)     : v
-    StaggeredZ,     // (i,     j,     k+1/2) : w
-
-    StaggeredYZ,    // (i,     j+1/2, k+1/2) : xi
-    StaggeredXZ,    // (i+1/2, j,     k+1/2) : eta
-    StaggeredXY,    // (i+1/2, j+1/2, k)     : zeta
-
-    StaggeredXYZ,   // (i+1/2, j+1/2, k+1/2)
-    Surface         // At surface
-};
 
 struct FieldMetadata {
     std::string units;
@@ -50,32 +35,6 @@ struct FieldMetadata {
           standard_name(std::move(standard_name_in)),
           comment(std::move(comment_in)) {}
 };
-
-inline const char* grid_staggering_to_string(GridStaggering staggering) noexcept {
-    switch (staggering) {
-        case GridStaggering::Unspecified:
-            return "unspecified";
-        case GridStaggering::NotApplicable:
-            return "not_applicable";
-        case GridStaggering::Centered:
-            return "centered";
-        case GridStaggering::StaggeredX:
-            return "staggered_x";
-        case GridStaggering::StaggeredY:
-            return "staggered_y";
-        case GridStaggering::StaggeredZ:
-            return "staggered_z";
-        case GridStaggering::StaggeredYZ:
-            return "staggered_yz";
-        case GridStaggering::StaggeredXZ:
-            return "staggered_xz";
-        case GridStaggering::StaggeredXY:
-            return "staggered_xy";
-        case GridStaggering::StaggeredXYZ:
-            return "staggered_xyz";
-    }
-    return "unspecified";
-}
 
 // Helper to create Kokkos::View of varying dimensions
 template<size_t Dim, typename ScalarType = VVM::Real>
