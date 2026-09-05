9fdcd475f9deba455afae2425fc02f10d98ae956
Aaron@blaze <b08209006@ntu.edu.tw>
2026-09-05 13:32:20 +0800

    feat: expose discrete Laplace-Beltrami diagonal

--- src/dynamics/operators/HorizontalLaplaceBeltrami.hpp
+++ src/dynamics/operators/HorizontalLaplaceBeltrami.hpp
@@ -21,12 +21,9 @@ struct HorizontalLaplaceBeltramiDeviceView {
         const ContravariantGradientAroundT gradient_around_t = gradient.calculate_uv_around_t(j, i, scalar);
 
         return divergence.at_t(
-            j,
-            i,
-            gradient_around_t.q1_at_u_j_i,
-            gradient_around_t.q1_at_u_j_im1,
-            gradient_around_t.q2_at_v_j_i,
-            gradient_around_t.q2_at_v_jm1_i);
+            j, i,
+            gradient_around_t.q1_at_u_j_i, gradient_around_t.q1_at_u_j_im1,
+            gradient_around_t.q2_at_v_j_i, gradient_around_t.q2_at_v_jm1_i);
     }
 
     template<typename ScalarView>
@@ -39,6 +36,20 @@ struct HorizontalLaplaceBeltramiDeviceView {
     KOKKOS_INLINE_FUNCTION
     VVM::Real calculate_at_t(const ScalarView& scalar, const int k, const int j, const int i) const noexcept {
         return calculate_at_t(j, i, load_scalar_stencil_at_t(scalar, k, j, i));
+    }
+
+    // Return the coefficient multiplying the T-point center value in the
+    // discrete nine-point Laplace-Beltrami stencil. The cross-metric terms
+    // contribute to the corner coefficients, but not to this diagonal.
+    KOKKOS_INLINE_FUNCTION
+    VVM::Real diagonal_at_t(const int j, const int i) const noexcept {
+        const VVM::Real inverse_dq1_squared = VVM::real(1.0) / (divergence.t.dq1 * divergence.t.dq1);
+        const VVM::Real inverse_dq2_squared = VVM::real(1.0) / (divergence.t.dq2 * divergence.t.dq2);
+
+        const VVM::Real q1_diagonal = (divergence.u.sqrt_g_g_contra.a11(j, i) + divergence.u.sqrt_g_g_contra.a11(j, i - 1)) * inverse_dq1_squared;
+        const VVM::Real q2_diagonal = (divergence.v.sqrt_g_g_contra.a22(j, i) + divergence.v.sqrt_g_g_contra.a22(j - 1, i)) * inverse_dq2_squared;
+
+        return -divergence.t.inv_sqrt_g(j, i) * (q1_diagonal + q2_diagonal);
     }
 };
 
